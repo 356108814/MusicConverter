0bdd9e3f7817efd7e3368da2c51a69275cb683dd
忧里 修斯 <769435570@qq.com>
2017-12-14 10:59:24 +0800

    quanmingktv

--- handlers/download.py
+++ handlers/download.py
@@ -11,15 +11,17 @@ class DownloadHandler(RequestHandler):
         name = self.get_argument("name")
         url = str(self.get_argument("url"))
         ext = os.path.splitext(url)[1]
+        if url.find("aliyuncs") != -1:
+            headers = {
+                'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat QBCore/3.43.691.400 QQBrowser/9.0.2524.400"}
+            ext = ".mp4"
+        else:
+            headers = {}
         # 中文名附件必须quote
         filename = urllib.parse.quote(name + ext)
         self.set_header('Content-Type', 'application/octet-stream;')
         self.set_header('Content-Disposition', 'attachment; filename=%s' % filename)
-        if url.find("aliyuncs") != -1:
-            headers = {
-                'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat QBCore/3.43.691.400 QQBrowser/9.0.2524.400"}
-        else:
-            headers = {}
+        
         req = request.Request(url, headers=headers)
         f = request.urlopen(req)
         self.write(f.read())
